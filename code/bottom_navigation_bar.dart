373ab7ee00bb9c04939adcd6fe6049c5621f5544
laohan <120936337@qq.com>
2018-12-27 20:50:03 +0800

    添加代码

--- code/bottom_navigation_bar.dart
+++ code/bottom_navigation_bar.dart
@@ -20,28 +20,6 @@ class BottomNav extends StatefulWidget {
 class _BottomNavState extends State<BottomNav> {
   int _currentIndex = 0;
 
-   final import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Study',
-      home: BottomNav(),
-    );
-  }
-}
-
-class BottomNav extends StatefulWidget {
-  @override
-  _BottomNavState createState() => _BottomNavState();
-}
-
-class _BottomNavState extends State<BottomNav> {
-  int _currentIndex = 0;
-
   final _widgetOptions = [
     Text('Index 0: Home'),
     Text('Index 1: Business'),
@@ -61,37 +39,6 @@ class _BottomNavState extends State<BottomNav> {
       body: Center(
         child: _widgetOptions.elementAt(_currentIndex),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home), title: Text('Home')),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.business), title: Text('Business')),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.school), title: Text('School')),
-          ],
-          fixedColor: Colors.green,
-          currentIndex: _currentIndex,
-          onTap: onItemTapped),
-    );
-  }
-}
-_widgetOptions = [
-   Text('Index 0: Home'),
-   Text('Index 1: Business'),
-   Text('Index 2: School'),
- ];
-
-  void onItemTapped(int idx) {
-    setState(() {
-      _currentIndex = idx;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Bottom Navigation Demo')),
       bottomNavigationBar: BottomNavigationBar(
           items: [
             BottomNavigationBarItem(
